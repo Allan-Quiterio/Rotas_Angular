7e32d8579596c47f683cfd3378583cb0fb4cf593
Allan-Quiterio <allanquiterio100@gmail.com>
2024-04-18 10:43:47 -0300

    feat: Configurando rotas simples

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { ModuleWithProviders } from '@angular/core';
 
+//Definindo todas as rotas
+const AppRoutingModule: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'cursos', component: CursosComponent },
+  { path: '', component: HomeComponent },
+];
 
-const routes: Routes = [];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+//Essa constante é a que contém as definições e configurações das nossas rotas em todo o projeto
+export const Routing: ModuleWithProviders =
+  RouterModule.forRoot(AppRoutingModule);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { Routing } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, CursosComponent, HomeComponent, LoginComponent],
+  imports: [BrowserModule, Routing],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
