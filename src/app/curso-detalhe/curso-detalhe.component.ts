0b92661c6f42af6074c1347a36f91fd32d62cd8d
Allan-Quiterio <allanquiterio100@gmail.com>
2024-04-18 20:06:41 -0300

    feat: Escutando mudanças nos parâmetros de roteamento

--- src/app/curso-detalhe/curso-detalhe.component.ts
+++ src/app/curso-detalhe/curso-detalhe.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-curso-detalhe',
@@ -8,9 +9,22 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class CursoDetalheComponent implements OnInit {
   id: string;
+  inscricao: Subscription;
+
   constructor(private route: ActivatedRoute) {
-    this.id = this.route.snapshot.params['id'];
+    // Essa é uma maneira de obter o resultado, porém ela não é dinâmica, só obtém quando o componente é construído, porém se mudar de valor, não é alterado
+    // this.id = this.route.snapshot.params['id'];
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // Já aqui temos uma forma dinâmica de obter através do params o id, fazendo uma inscrição nesse cara
+    this.inscricao = this.route.params.subscribe((params: any) => {
+      this.id = params['id'];
+    });
+  }
+
+  //Por boas práticas é interessante parar essa inscrição depois que o componente for destruído no atributo
+  ngOnDestroy() {
+    this.inscricao.unsubscribe();
+  }
 }
